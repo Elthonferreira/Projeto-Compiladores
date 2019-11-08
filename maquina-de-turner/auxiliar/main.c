1eec3d3d056a85fba2174cfbde48c0ce2a8d1928
Elthon Ferreira <elthonferreira18@gmail.com>
2019-11-08 16:32:45 -0300

    maquina de turner

---
+++ maquina-de-turner/auxiliar/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 1865;
+    for (int i = 1865; i<2040; i++){
+    printf("%d.\n\n", i);
+    }
+    return 0;
+}
---
+++ maquina-de-turner/teste/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i = 0;
+
+   for (i = 5; i <= 110; i++){
+        printf("(default, , %d),\n", i);
+   }
+}
